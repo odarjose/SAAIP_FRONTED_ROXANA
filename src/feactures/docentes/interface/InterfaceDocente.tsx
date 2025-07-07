4a82a91ae8824a4949b87c69ec7308595923f010
odar <odarjimenezj@gmail.com>
2025-07-06 23:53:30 -0500

    Final

--- src/feactures/docentes/interface/InterfaceDocente.tsx
+++ src/feactures/docentes/interface/InterfaceDocente.tsx
@@ -18,7 +18,7 @@ export interface RegistrarDocente extends Usuario {
     tipo_contrato: TipoContrato;
     fecha_inicio_contrato: string; // formato ISO
     fecha_fin_contrato: string; // formato ISO
-    estado?: boolean;
+    estado?: "activo" | "inactivo";
 }
 // Respuesta del backend para operaciones GET
 export interface DocenteResponseDTO extends Usuario {
@@ -28,7 +28,7 @@ export interface DocenteResponseDTO extends Usuario {
   tipo_contrato: TipoContrato;
   fecha_inicio_contrato: string;
   fecha_fin_contrato: string;
-  estado: boolean;
+  estado: "activo" | "inactivo" | undefined;
 }
 /** Esto es para actualizar solo se enviaran los campo donde se dectete modificacion */
 export type PartialDocente = Partial<Omit<RegistrarDocente, 'id_docente'>> & {
